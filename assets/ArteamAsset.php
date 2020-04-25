5f454ed615dc5adaf4cb432f29264dfa55746440
Rave <rudder.rave@gmail.com>
2020-04-25 22:20:19 +0300

    Change to bootstrap 4

--- assets/ArteamAsset.php
+++ assets/ArteamAsset.php
@@ -13,27 +13,28 @@ use yii\web\JqueryAsset;
 class ArteamAsset extends AssetBundle
 {
 
-    public function init()
-    {
-        $this->sourcePath = __DIR__ . '/admin';
+    public $sourcePath = '@vendor/almasaeed2010/adminlte';
+    public $css = [
+        'dist/css/adminlte.min.css',
+        'arteam/assets/admin/css/site.css'
+    ];
 
-        $this->js = [
-            'js/admin.js',
-        ];
+    public $js = [
+        'dist/js/adminlte.js',
+        'plugins/bootstrap/js/bootstrap.min.js'
+    ];
 
-        $this->css = [
-            'css/admin.css',
-            'css/widget.css',
-            'css/styler.css',
-        ];
+    public $publishOptions = [
+        "only" => [
+            "dist/js/*",
+            "dist/css/*",
+            "plugins/bootstrap/js/*",
+        ],
 
-        $this->depends = [
-            JqueryAsset::className(),
-            'yii\web\YiiAsset',
-            'yii\bootstrap\BootstrapAsset',
-            'rmrevin\yii\fontawesome\AssetBundle',
-        ];
+    ];
 
-        parent::init();
-    }
+    public $depends = [
+        'yii\web\YiiAsset',
+        'arteam\assets\FontAwesomeAsset'
+    ];
 }
